7abdee3d6e852fc5ea4ab9a4b6fa15383accc85c
BAPTISTE NEVEJANS <baptisrenevejans7@gmai.com>
2022-04-26 14:49:10 +0200

    mise à jour menu

--- ActionMenu2.java
+++ ActionMenu2.java
@@ -1,29 +1,31 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import javax.swing. *;
+import java.awt. *;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ActionMenu2  extends JFrame implements TextListener {
 
-    JTextArea newligne;
-    JTextArea newcolonne;
-    JTextArea newnbrMines;
+    JTextField newligne;
+    JTextField newcolonne;
+    JTextField newnbrMines;
 
-    public void ActionMenu2(JTextArea newligne,JTextArea newcolonne,JTextArea newnbrMines){
+    public void ActionMenue2(JTextField newligne,JTextField newcolonne,JTextField newnbrMines){
              this.newligne=newligne;
              this.newcolonne=newcolonne;
-             this.newnbrMines=newnbrMines;   
+             this.newnbrMines=newnbrMines;
+    }
 
     public void actionPerformed(TextEvent xmenu2) {
         if (xmenu2.getSource() == newligne){
-            int ligne = Integer.parseInt(newligne);
+            int ligne = Integer.parseInt(newligne.getText());
         }
         if (xmenu2.getSource()== newcolonne){
-            int colonne = Integer.parseInt(newcolonne);
+            int colonne = Integer.parseInt(newcolonne.getText());
         }
         if (xmenu2.getSource()== newnbrMines){
-            int nbrMines = Integer.parseInt(newnbrMines);
+            int nbrMines = Integer.parseInt(newnbrMines.getTest());
 
         }
     }
     }
-    }
+ 
