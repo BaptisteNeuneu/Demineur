fbb8199e868b444ce2b7f60b302db0dd4a2c4248
brinet <brinet@dwarves.iut-fbleau.fr>
2022-05-12 13:23:47 +0200

    delete files

--- MainFenetre.java
+++
@@ -1,9 +0,0 @@
-/**
-* @version 26/04/2022
-* @author Felix Brinet
-*/
-public class MainFenetre{
-    public static void main(String[] args){
-    new Fenetre();
-}
-}
--- doClose.java
+++
@@ -1,9 +0,0 @@
-/**
-* @version 09/05/2022
-* @author Baptiste Nevejans
-*/
-public class doClose extends Test {
-    public void fermer(){
-   System.exit(0);
-}
-}
